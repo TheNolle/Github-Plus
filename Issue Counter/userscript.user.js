d742dd2b1fc2e10773c730636e1d55363a3503a7
TheNolle <61505071+TheNolle@users.noreply.github.com>
2020-08-13 16:51:49 +0200

    Updated URLs
    Updated the Download URL, the Update URL and the Support URL.

--- Issue Counter/userscript.user.js
+++ Issue Counter/userscript.user.js
@@ -9,13 +9,14 @@
 // @author       TheNolle Studios
 // @description           Improves Github in a way you may like it.
 
-// @supportURL   https://github.com/TheNolle/Spotify-Adblocker/issues/new/choose
-// @updateURL    https://github.com/TheNolle/Spotify-Adblocker/raw/master/userscript.user.js
-// @downloadURL  https://github.com/TheNolle/Spotify-Adblocker/raw/master/userscript.user.js
+// @supportURL   https://github.com/TheNolle/Github-Plus/issues/new/choose
+// @updateURL    https://github.com/TheNolle/Github-Plus/raw/master/Issue%20Counter/userscript.user.js
+// @downloadURL  https://github.com/TheNolle/Github-Plus/raw/master/Issue%20Counter/userscript.user.js
 
 // @include     https://github.com/*
 
 // @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=666427
+
 // @connect     api.github.com
 
 // @grant       GM_addStyle
