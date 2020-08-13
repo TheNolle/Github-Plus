26b68b59e03e2dd7c3a1698d85d1b2b8cb8ba430
TheNolle <61505071+TheNolle@users.noreply.github.com>
2020-08-13 16:56:54 +0200

    Update userscript.user.js

--- Show Whitespaces/userscript.user.js
+++ Show Whitespaces/userscript.user.js
@@ -9,9 +9,9 @@
 // @author       TheNolle Studios
 // @description           Improves Github in a way you may like it.
 
-// @supportURL   https://github.com/TheNolle/Spotify-Adblocker/issues/new/choose
-// @updateURL    https://github.com/TheNolle/Spotify-Adblocker/raw/master/userscript.user.js
-// @downloadURL  https://github.com/TheNolle/Spotify-Adblocker/raw/master/userscript.user.js
+// @supportURL   https://github.com/TheNolle/Github-Plus/issues/new/choose
+// @updateURL    https://github.com/TheNolle/Github-Plus/raw/master/Show%20Whitespaces/userscript.user.js
+// @downloadURL  https://github.com/TheNolle/Github-Plus/raw/master/Show%20Whitespaces/userscript.user.js
 
 // @include     https://github.com/*
 // @include     https://gist.github.com/*
@@ -29,7 +29,7 @@
 // @grant       GM.registerMenuCommand
 
 // @run-at      document-idle
-// ==/UserScript==
+// ==UserScript==
 
   // Create a new localStorage + Displays an Alert Box on First Use
     var FirstUse = localStorage.getItem('FirstUse') || '';
